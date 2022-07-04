59df0b2ca83bdf690f99f1afa945cf816937ac6f
Swapnil639 <swapnilpatil631997@gmail.com>
2022-07-04 23:35:15 +0530

    Added :Refactored code to throw custom exception

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -3,20 +3,42 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public boolean validateFirstName(String firstName){
-        return Pattern.matches("[A-Z][A-Z a-z]{2,}",firstName);
+    public boolean validateFirstName(String firstName) throws UserRegistrationException {
+        if (Pattern.matches("[A-Z][a-z A-Z]{2,}", firstName))
+            return true;
+        else
+            throw new UserRegistrationException("Invalid First Name");
     }
-    public boolean validateLastName(String lastName){
-        return Pattern.matches("[A-Z][A-Z a-z]{2,}",lastName);
+
+    public boolean validateLastname(String lastName) throws UserRegistrationException{
+        if (Pattern.matches("[A-Z][a-z A-Z]{2,}", lastName))
+            return true;
+        else
+            throw new UserRegistrationException("Invalid Last Name");
+
     }
-    public boolean validateEmail(String email){
-        return Pattern.matches("[\\w+-]+(?:\\.[\\w+-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}",email);
+
+    public boolean validateEmail(String email) throws UserRegistrationException {
+        if (Pattern.matches("[\\w+-]+(?:\\.[\\w+-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}", email))
+            return true;
+        else
+            throw new UserRegistrationException("Invalid Email");
     }
-    public boolean validateMobileNumber(String mobileNumber){
-        return Pattern.matches("[+]91 [6-9]\\d{9}",mobileNumber);
+
+
+    public boolean validateMobileNumber(String mobileNumber) throws UserRegistrationException {
+        if (Pattern.matches("[+]91 [6-9]\\d{9}", mobileNumber))
+            return true;
+        else
+            throw new UserRegistrationException("Invalid mobile number");
     }
-    public boolean validatePassword(String password){
-        return Pattern.matches("(?=.*[A-Z])(?=.*[0-9])(?=.*[$&+,:;=?@#|'<>.-^*()%!])[a-z A-Z 0-9 $&+,:;=?@#|'<>.-^*()%!]{8,}",password);
+
+
+    public boolean validatePassword(String password) throws UserRegistrationException {
+        if (Pattern.matches("(?=.*[A-Z])(?=.*\\d)(?=.*[$&+,:;=?@#|'<>.-^*()%!])[a-z A-Z \\d $&+,:;=?@#|'<>.-^*()%!]{8,}", password))
+            return true;
+        else
+            throw new UserRegistrationException("Invalid Password");
     }
 
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -7,100 +7,53 @@ public class UserRegistrationTest {
     public UserRegistration userRegistration = new UserRegistration();
 
     @Test
-    public void validateFirstNameTestReturnTrue(){
-        boolean output = userRegistration.validateFirstName("Swapnil");
-        Assert.assertTrue(output);
+    public void validateFirstNameTestReturnTrue() {
+        try {
+            boolean output = userRegistration.validateFirstName("Swapnil");
+            Assert.assertTrue(output);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
+
     @Test
-    public void validateFirstNameTestReturnFalse(){
-        boolean output = userRegistration.validateFirstName("swappy");
-        Assert.assertFalse(output);
+    public void validateLastNameTestReturnTrue() {
+        try {
+            boolean output = userRegistration.validateLastname("Patil");
+            Assert.assertTrue(output);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-    @Test
-    public void validateLastNameTestReturnTrue(){
-        boolean output = userRegistration.validateLastName("Patil");
-        Assert.assertTrue(output);
-    }
-    @Test
-    public void validateLastNameTestReturnFalse(){
-        boolean output = userRegistration.validateLastName("patil");
-        Assert.assertFalse(output);
-    }
+
     @Test
     public void validateEmailTestReturnTrue() {
-        boolean output = userRegistration.validateEmail("swapnilpatil631997@gmail.com");
-        Assert.assertTrue(output);
+        try {
+            boolean output = userRegistration.validateEmail("swapnilpatil631997@gmail.com");
+            Assert.assertTrue(output);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-    @Test
-    public void validateEmailTestReturnFalse() {
-        boolean output = userRegistration.validateEmail("SwapnilPatil.com");
-        Assert.assertFalse(output);
-    }
+
     @Test
     public void validateMobileNumberReturnTrue() {
-        boolean output = userRegistration.validateMobileNumber("+91 9967891679");
-        Assert.assertTrue(output);
-    }@Test
-    public void validateMobileNumberReturnFalse() {
-        boolean output = userRegistration.validateMobileNumber("9967891679");
-        Assert.assertFalse(output);
+        try {
+            boolean output = userRegistration.validateMobileNumber("+91 9967891679");
+            Assert.assertTrue(output);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
+
     @Test
     public void validatePasswordReturnTrue() {
-        boolean output = userRegistration.validatePassword("SuccessKey639");
-        Assert.assertTrue(output);
-    }
-    @Test
-    public void validatePasswordReturnFalse() {
-        boolean output = userRegistration.validatePassword("successkey123");
-        Assert.assertFalse(output);
-    }
-    @Test
-    public void validEmailValidationTest(){
-        boolean output1 = userRegistration.validateEmail("abc@yahoo.com");
-        Assert.assertTrue(output1);
-        boolean output2 = userRegistration.validateEmail("abc-100@yahoo.com");
-        Assert.assertTrue(output2);
-        boolean output3 = userRegistration.validateEmail("abc.100@yahoo.com");
-        Assert.assertTrue(output3);
-        boolean output4 = userRegistration.validateEmail("abc111@abc.com");
-        Assert.assertTrue(output4);
-        boolean output5 = userRegistration.validateEmail("abc-100@abc.net");
-        Assert.assertTrue(output5);
-        boolean output6 = userRegistration.validateEmail("abc@1.com");
-        Assert.assertTrue(output6);
-        boolean output7 = userRegistration.validateEmail("abc+100@gmail.com");
-        Assert.assertTrue(output7);
-    }
+        try {
+            boolean output = userRegistration.validatePassword("Successkey123");
+            Assert.assertTrue(output);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
-    @Test
-    public void invalidEmailValidationTest(){
-        boolean output1 = userRegistration.validateEmail("abc");
-        Assert.assertFalse(output1);
-        boolean output2 = userRegistration.validateEmail("abc@.com.my");
-        Assert.assertFalse(output2);
-        boolean output3 = userRegistration.validateEmail("abc123@gmail.a");
-        Assert.assertFalse(output3);
-        boolean output4 = userRegistration.validateEmail("abc123@.com");
-        Assert.assertFalse(output4);
-        boolean output5 = userRegistration.validateEmail("abc123@.com.com");
-        Assert.assertFalse(output5);
-        boolean output6 = userRegistration.validateEmail("abc@abc..com");
-        Assert.assertFalse(output6);
-        boolean output7 = userRegistration.validateEmail("abc()*@gmail.com");
-        Assert.assertFalse(output7);
-        boolean output8 = userRegistration.validateEmail("abc@%*.com");
-        Assert.assertFalse(output8);
-        boolean output9 = userRegistration.validateEmail("abc..2002@gmail.com");
-        Assert.assertFalse(output9);
-        boolean output10 = userRegistration.validateEmail("abc.@gmail.com");
-        Assert.assertFalse(output10);
-        boolean output11 = userRegistration.validateEmail("abc@abc@gmail.com");
-        Assert.assertFalse(output11);
-        boolean output12 = userRegistration.validateEmail("abc@gamil.com.1a");
-        Assert.assertFalse(output12);
-        boolean output13 = userRegistration.validateEmail("abc@gmail.com.aa.au");
-        Assert.assertFalse(output13);
     }
-
 }
