b411f75a7de15bf0468f6a8c01304bfa05bc3807
Swapnil639 <swapnilpatil631997@gmail.com>
2022-07-06 23:24:58 +0530

    Refactor :Lambda function used to validate user's deatils

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -2,43 +2,45 @@ package com.bridgelabz;
 
 import java.util.regex.Pattern;
 
+@FunctionalInterface
+interface UserDetailsValidationFI {
+    boolean validate(String x) throws UserRegistrationException;
+}
+
 public class UserRegistration {
-    public boolean validateFirstName(String firstName) throws UserRegistrationException {
+    UserDetailsValidationFI validateFirstName = firstName -> {
+        // regex pattern for firstname
         if (Pattern.matches("[A-Z][a-z A-Z]{2,}", firstName))
             return true;
         else
             throw new UserRegistrationException("Invalid First Name");
-    }
+    };
 
-    public boolean validateLastname(String lastName) throws UserRegistrationException{
+    UserDetailsValidationFI validateLastName = lastName -> {
         if (Pattern.matches("[A-Z][a-z A-Z]{2,}", lastName))
             return true;
         else
             throw new UserRegistrationException("Invalid Last Name");
+    };
 
-    }
-
-    public boolean validateEmail(String email) throws UserRegistrationException {
+    UserDetailsValidationFI validateEmail = email -> {
         if (Pattern.matches("[\\w+-]+(?:\\.[\\w+-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}", email))
             return true;
         else
             throw new UserRegistrationException("Invalid Email");
-    }
+    };
 
-
-    public boolean validateMobileNumber(String mobileNumber) throws UserRegistrationException {
+    UserDetailsValidationFI validateMobileNumber = mobileNumber -> {
         if (Pattern.matches("[+]91 [6-9]\\d{9}", mobileNumber))
             return true;
         else
-            throw new UserRegistrationException("Invalid mobile number");
-    }
+            throw new UserRegistrationException("Invalid Mobile Number");
+    };
 
-
-    public boolean validatePassword(String password) throws UserRegistrationException {
+    UserDetailsValidationFI validatePassword = password -> {
         if (Pattern.matches("(?=.*[A-Z])(?=.*\\d)(?=.*[$&+,:;=?@#|'<>.-^*()%!])[a-z A-Z \\d $&+,:;=?@#|'<>.-^*()%!]{8,}", password))
             return true;
         else
             throw new UserRegistrationException("Invalid Password");
-    }
-
+    };
 }
